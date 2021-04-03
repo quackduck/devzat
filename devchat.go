e2b31581d99ea73626e6fd4dc756d6be47cd88de
Ishan Goel <igoel.mail@gmail.com>
2021-04-03 14:08:49 +0400

    Better

--- devchat.go
+++ devchat.go
@@ -2,28 +2,42 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/crypto/ssh/terminal"
+	"math/rand"
 	"strings"
+	"time"
 
 	//"golang.org/x/term"
 	"io"
 	"log"
 	"os"
 
+	"github.com/fatih/color"
 	"github.com/gliderlabs/ssh"
+	terminal "golang.org/x/term"
 )
 
 func main() {
 	writers := make([]func(a ...interface{}), 0, 5)
 	users := make([]string, 0, 5)
+	red := color.New(color.FgHiRed)
+	green := color.New(color.FgHiGreen)
+	cyan := color.New(color.FgHiCyan)
+	magenta := color.New(color.FgHiMagenta)
+	yellow := color.New(color.FgHiYellow)
+
+	colorArr := []*color.Color{red, green, cyan, magenta, yellow}
 
 	ssh.Handle(func(s ssh.Session) {
-		username := s.User()
+		myColor := *colorArr[rand.Intn(len(colorArr))]
+		username := myColor.Sprint(s.User())
 		term := terminal.NewTerminal(s, "> ")
+		term.SetSize(2000, 2000) // disable any formatting done by term
+		rand.Seed(time.Now().Unix())
+
 		writeln := func(a ...interface{}) {
 			msg := fmt.Sprintln(a...)
 			if !strings.HasPrefix(msg, username) {
-				term.Write([]byte(msg))
+				term.Write([]byte(time.Now().Format(time.Kitchen) + " " + msg))
 			}
 		}
 		sendMsg := func(a ...interface{}) {
@@ -47,7 +61,7 @@ func main() {
 
 		for userDuplicate(username) {
 			var err error
-			writeln("Pick a different username:")
+			writeln("Pick a different username")
 			username, err = term.ReadLine()
 			if err != nil {
 				writeln(err)
@@ -60,8 +74,16 @@ func main() {
 		defer func() { users = remove(users, username) }()
 
 		//reader := bufio.NewReader(s)
-		writeln("Welcome to the chat", "There are", len(users)-1, "more users")
-		sendMsg(username, "has joined the chat")
+		switch len(users) - 1 {
+		case 0:
+			writeln(cyan.Sprint("Welcome to the chat. There are no more users"))
+		case 1:
+			writeln(cyan.Sprint("Welcome to the chat. There is one more user"))
+		default:
+			writeln(cyan.Sprint("Welcome to the chat. There are ", len(users)-1, " more users"))
+		}
+
+		sendMsg(username + green.Sprint(" has joined the chat"))
 		//b := make([]byte, 12)
 		//_, err := s.Read(b)
 		//if err != nil {
@@ -81,7 +103,7 @@ func main() {
 			line = strings.TrimSpace(line)
 
 			if err == io.EOF {
-				sendMsg(username + " has left the chat.")
+				sendMsg(username + red.Sprint(" has left the chat."))
 				return
 			}
 			if err != nil {
@@ -98,20 +120,24 @@ func main() {
 			//	log.Println(err)
 			//	continue
 			//}
-			sendMsg(username + ": " + line)
+			if !(line == "") {
+				sendMsg(username + ": " + line)
+			}
+			if line == "/users" {
+				sendMsg(users)
+			}
 		}
 	})
 	fmt.Println("Starting chat server on port 2222")
 	log.Fatal(ssh.ListenAndServe(":2222", nil, ssh.HostKeyFile(os.Getenv("HOME")+"/.ssh/id_rsa")))
 }
 
-func remove(slice []string, a string) []string {
+func remove(s []string, a string) []string {
 	var i int
-	for i = range slice {
-		if slice[i] == a {
+	for i = range s {
+		if s[i] == a {
 			break // i is now where it is
 		}
 	}
-	slice[i] = slice[len(slice)-1]
-	return slice[:len(slice)-1]
+	return append(s[:i], s[i+1:]...)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/fatih/color v1.10.0
 	github.com/gliderlabs/ssh v0.3.2
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
 )
