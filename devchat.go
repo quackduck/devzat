d6076f71947973d5d5e20b837bdf2b4803bd7d96
Ishan Goel <igoel.mail@gmail.com>
2021-04-08 14:08:01 +0400

    Huge refactor plus new commands

--- devchat.go
+++ devchat.go
@@ -3,26 +3,32 @@ package main
 import (
 	"crypto/sha256"
 	_ "embed"
+	"encoding/gob"
 	"encoding/hex"
 	"fmt"
+	gossh "golang.org/x/crypto/ssh"
 	"io"
 	"math/rand"
 	"os"
+	"os/signal"
 	"strconv"
 	"strings"
+	"sync"
+	"syscall"
 	"time"
 
 	"github.com/acarl005/stripansi"
 	"github.com/fatih/color"
 	"github.com/gliderlabs/ssh"
 	"github.com/slack-go/slack"
-	gossh "golang.org/x/crypto/ssh"
 	terminal "golang.org/x/term"
 )
 
 var (
 	//go:embed slackAPI.txt
-	slackAPI   []byte
+	slackAPI []byte
+	//go:embed adminPass.txt
+	adminPass  []byte
 	slackChan  = getSendToSlackChan()
 	api        = slack.New(string(slackAPI))
 	rtm        = api.NewRTM()
@@ -34,8 +40,9 @@ var (
 	blue       = color.New(color.FgHiBlue)
 	black      = color.New(color.FgHiBlack)
 	white      = color.New(color.FgHiWhite)
-	colorArr   = []*color.Color{green, cyan, magenta, yellow, white}
+	colorArr   = []*color.Color{green, cyan, magenta, yellow, white, blue}
 	users      = make([]*user, 0, 10)
+	allUsers   = make(map[string]string, 100) //map format is u.id => u.name
 	port       = 22
 	scrollback = 16
 	backlog    = make([]string, 0, scrollback)
@@ -55,11 +62,172 @@ func broadcast(msg string, toSlack bool) {
 }
 
 type user struct {
-	name    string
-	session ssh.Session
-	term    *terminal.Terminal
-	bell    bool
-	color   color.Color
+	name      string
+	session   ssh.Session
+	term      *terminal.Terminal
+	bell      bool
+	color     color.Color
+	id        string
+	closeOnce sync.Once
+}
+
+func newUser(s ssh.Session) *user {
+	term := terminal.NewTerminal(s, "> ")
+	_ = term.SetSize(10000, 10000) // disable any formatting done by term
+
+	hash := sha256.New()
+	hash.Write([]byte(strings.TrimSpace(string(gossh.MarshalAuthorizedKey(s.PublicKey())))))
+	u := &user{"", s, term, true, color.Color{}, hex.EncodeToString(hash.Sum(nil)), sync.Once{}}
+	u.pickUsername(s.User())
+	users = append(users, u)
+	allUsers[u.id] = u.name
+	switch len(users) - 1 {
+	case 0:
+		u.writeln(cyan.Sprint("Welcome to the chat. There are no more users"))
+	case 1:
+		u.writeln(cyan.Sprint("Welcome to the chat. There is one more user"))
+	default:
+		u.writeln(cyan.Sprint("Welcome to the chat. There are ", len(users)-1, " more users"))
+	}
+	_, _ = term.Write([]byte(strings.Join(backlog, ""))) // print out backlog
+
+	broadcast(u.name+green.Sprint(" has joined the chat"), true)
+	return u
+}
+
+func (u *user) repl() {
+	for {
+		line, err := u.term.ReadLine()
+		line = strings.TrimSpace(line)
+
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			u.writeln(fmt.Sprint(err))
+			fmt.Println(u.name, err)
+			continue
+		}
+		toSlack := true
+
+		if strings.HasPrefix(line, "/hide") {
+			toSlack = false
+		}
+		if !(line == "") {
+			broadcast(u.name+": "+line, toSlack)
+		} else {
+			u.writeln("An empty message? Send some content!")
+			continue
+		}
+		if line == "/users" {
+			names := make([]string, 0, len(users))
+			for _, us := range users {
+				names = append(names, us.name)
+			}
+			broadcast(fmt.Sprint(names), toSlack)
+		}
+		if line == "/all" {
+			names := make([]string, 0, len(allUsers))
+			for _, name := range allUsers {
+				names = append(names, name)
+			}
+			broadcast(fmt.Sprint(names), toSlack)
+		}
+		if line == "easter" {
+			broadcast("eggs?", toSlack)
+		}
+		if line == "/exit" {
+			return
+		}
+		if line == "/bell" {
+			u.bell = !u.bell
+			broadcast(fmt.Sprint("bell: ", u.bell), toSlack)
+		}
+		if strings.HasPrefix(line, "/id") {
+			victim, ok := findUserByName(strings.TrimSpace(strings.TrimPrefix(line, "/id")))
+			if !ok {
+				broadcast("User not found", toSlack)
+			} else {
+				broadcast(victim.id, toSlack)
+			}
+		}
+		if strings.HasPrefix(line, "/nick") {
+			u.pickUsername(strings.TrimSpace(strings.TrimPrefix(line, "/nick")))
+		}
+		if strings.HasPrefix(line, "/kick") {
+			victim, ok := findUserByName(strings.TrimSpace(strings.TrimPrefix(line, "/kick")))
+			if !ok {
+				broadcast("User not found", toSlack)
+			} else {
+				var pass string
+				pass, err = u.term.ReadPassword("Admin password: ")
+				if err != nil {
+					fmt.Println(u.name, err)
+				}
+				if strings.TrimSpace(pass) == strings.TrimSpace(string(adminPass)) {
+					victim.close(true, u)
+				} else {
+					broadcast("Incorrect password", toSlack)
+				}
+			}
+		}
+		if strings.HasPrefix(line, "/color") {
+			colorMsg := "Which color? Choose from green, cyan, blue, red (orange), magenta (purple, pink), yellow (beige), white (cream) and black (gray, grey)."
+			switch strings.TrimSpace(strings.TrimPrefix(line, "/color")) {
+			case "green":
+				u.changeColor(*green)
+			case "cyan":
+				u.changeColor(*cyan)
+			case "blue":
+				u.changeColor(*blue)
+			case "red", "orange":
+				u.changeColor(*red)
+			case "magenta", "purple", "pink":
+				u.changeColor(*magenta)
+			case "yellow", "beige":
+				u.changeColor(*yellow)
+			case "white", "cream":
+				u.changeColor(*white)
+			case "black", "gray", "grey":
+				u.changeColor(*black)
+			case "easter":
+				u.changeColor(*color.New(color.BgHiMagenta, color.FgHiGreen))
+			case "baby":
+				u.changeColor(*color.New(color.BgBlue, color.FgHiMagenta))
+			case "l33t":
+				u.changeColor(*u.color.Add(color.BgHiBlack))
+			default:
+				broadcast(colorMsg, toSlack)
+			}
+		}
+		if line == "/help" {
+			broadcast(`Available commands:
+   /users   list users
+   /nick    change your name
+   /color   change your name color
+   /exit    leave the chat
+   /hide    hide messages from HC Slack
+   /bell    toggle the ascii bell
+   /id      get a unique identifier for a user
+   /all     get a list of all unique users ever
+   /kick    kick a user, requires an admin pass
+   /help    show this help message
+Made by Ishan Goel with feature ideas from Hack Club members.
+Thanks to Caleb Denio for lending me his server!`, toSlack)
+		}
+	}
+}
+
+func (u *user) close(kicked bool, kicker *user) {
+	u.closeOnce.Do(func() {
+		users = remove(users, u)
+		if kicked {
+			broadcast(u.name+red.Sprint(" has been kicked by ")+kicker.name, true)
+		} else {
+			broadcast(u.name+red.Sprint(" has left the chat"), true)
+		}
+		u.session.Close()
+	})
 }
 
 func (u *user) writeln(msg string) {
@@ -71,7 +239,9 @@ func (u *user) writeln(msg string) {
 		}
 	}
 }
-func (u *user) pickNewUsername(possibleName string) {
+
+func (u *user) pickUsername(possibleName string) {
+	possibleName = strings.TrimSpace(possibleName)
 	var err error
 	for userDuplicate(possibleName) {
 		u.writeln("Pick a different username")
@@ -80,6 +250,7 @@ func (u *user) pickNewUsername(possibleName string) {
 		if err != nil {
 			fmt.Println(err)
 		}
+		possibleName = strings.TrimSpace(possibleName)
 	}
 	u.color = *colorArr[rand.Intn(len(colorArr))]
 	possibleName = u.color.Sprint(possibleName)
@@ -103,134 +274,37 @@ func userDuplicate(a string) bool {
 	return false
 }
 
+func saveAllUsersToLog() {
+	f, err := os.Create("allUsers.gob")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	g := gob.NewEncoder(f)
+	g.Encode(allUsers)
+	f.Close()
+}
+
 func main() {
 	rand.Seed(time.Now().Unix())
-	var err error
+	f, err := os.Open("allUsers.gob")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	d := gob.NewDecoder(f)
+	d.Decode(&allUsers)
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		saveAllUsersToLog()
+		os.Exit(0)
+	}()
 	ssh.Handle(func(s ssh.Session) {
-		//myColor := *colorArr[rand.Intn(len(colorArr))]
-
-		term := terminal.NewTerminal(s, "> ")
-		_ = term.SetSize(10000, 10000) // disable any formatting done by term
-
-		u := &user{"", s, term, true, color.Color{}}
-		u.pickNewUsername(s.User())
-		users = append(users, u)
-
-		defer func() {
-			users = remove(users, u)
-			broadcast(u.name+red.Sprint(" has left the chat."), true)
-		}()
-		switch len(users) - 1 {
-		case 0:
-			u.writeln(cyan.Sprint("Welcome to the chat. There are no more users"))
-		case 1:
-			u.writeln(cyan.Sprint("Welcome to the chat. There is one more user"))
-		default:
-			u.writeln(cyan.Sprint("Welcome to the chat. There are ", len(users)-1, " more users"))
-		}
-		_, _ = term.Write([]byte(strings.Join(backlog, ""))) // print out backlog
-
-		broadcast(u.name+green.Sprint(" has joined the chat"), true)
-		var line string
-		for {
-			line, err = term.ReadLine()
-			line = strings.TrimSpace(line)
-
-			if err == io.EOF {
-				return
-			}
-			if err != nil {
-				u.writeln(fmt.Sprint(err))
-				fmt.Println(u.name, err)
-				continue
-			}
-			toSlack := true
-
-			if strings.HasPrefix(line, "/hide") {
-				toSlack = false
-			}
-			if !(line == "") {
-				broadcast(u.name+": "+line, toSlack)
-			} else {
-				u.writeln("An empty message? Send some content!")
-				continue
-			}
-			if line == "/users" {
-				names := make([]string, 0, len(users))
-				for _, us := range users {
-					names = append(names, us.name)
-				}
-				broadcast(fmt.Sprint(names), toSlack)
-			}
-			if line == "easter" {
-				broadcast("eggs?", toSlack)
-			}
-			if line == "/exit" {
-				return
-			}
-			if line == "/bell" {
-				u.bell = !u.bell
-				broadcast(fmt.Sprint("bell: ", u.bell), toSlack)
-			}
-			if strings.HasPrefix(line, "/id") {
-				victim, ok := findUserByName(strings.TrimSpace(strings.TrimPrefix(line, "/id")))
-				if !ok {
-					broadcast("User not found", toSlack)
-				} else {
-					hash := sha256.New()
-					hash.Write([]byte(strings.TrimSpace(string(gossh.MarshalAuthorizedKey(victim.session.PublicKey())))))
-					broadcast(hex.EncodeToString(hash.Sum(nil)), toSlack)
-				}
-			}
-			if strings.HasPrefix(line, "/nick") {
-				u.pickNewUsername(strings.TrimSpace(strings.TrimPrefix(line, "/nick")))
-			}
-			if strings.HasPrefix(line, "/color") {
-				colorMsg := "Which color? Choose from green, cyan, blue, red (orange), magenta (purple, pink), yellow (beige), white (cream) and black (gray, grey)."
-				//if len(parsed) < 2 {
-				//	broadcast(colorMsg, toSlack)
-				//} else {
-				switch strings.TrimPrefix(line, "/color") {
-				case "green":
-					u.changeColor(*green)
-				case "cyan":
-					u.changeColor(*cyan)
-				case "blue":
-					u.changeColor(*blue)
-				case "red", "orange":
-					u.changeColor(*red)
-				case "magenta", "purple", "pink":
-					u.changeColor(*magenta)
-				case "yellow", "beige":
-					u.changeColor(*yellow)
-				case "white", "cream":
-					u.changeColor(*white)
-				case "black", "gray", "grey":
-					u.changeColor(*black)
-				case "easter":
-					u.changeColor(*color.New(color.BgHiMagenta, color.FgHiGreen))
-				case "baby":
-					u.changeColor(*color.New(color.BgBlue, color.FgHiMagenta))
-				case "l33t":
-					u.changeColor(*u.color.Add(color.BgHiBlack))
-				default:
-					broadcast(colorMsg, toSlack)
-				}
-				//}
-			}
-			if line == "/help" {
-				broadcast(`Available commands:
-   /users   list users
-   /nick    change your name
-   /bell    toggle the ascii bell
-   /color   change your name color
-   /id      get a unique identifier for a user
-   /exit    leave the chat
-   /help    show this help message
-   /hide    hide messages from HC Slack
-Made by Ishan Goel (@quackduck)`, toSlack)
-			}
-		}
+		u := newUser(s)
+		u.repl()
+		u.close(false, nil)
 	})
 	if os.Getenv("PORT") != "" {
 		port, err = strconv.Atoi(os.Getenv("PORT"))
@@ -303,6 +377,9 @@ func remove(s []*user, a *user) []*user {
 		if s[i] == a {
 			break // i is now where it is
 		}
+	}
+	if i == 0 {
+		return make([]*user, 0)
 	}
 	return append(s[:i], s[i+1:]...)
 }
