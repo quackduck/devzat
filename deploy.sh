f438e88e69166e26463514262d415e3399632582
Ishan Goel <igoel.mail@gmail.com>
2021-04-18 17:43:03 +0400

    Update

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo Started
 #chmod 600 ~/.ssh/*
 scp -o StrictHostKeyChecking=no -P 4242 ./devchat.go go.sum go.mod ishan@34.75.6.116:~/devchat
 echo Copied files
-ssh -o StrictHostKeyChecking=no -T -p 4242 ishan@34.75.6.116 <<EOL # Unquote so lines are expanded
+ssh -o StrictHostKeyChecking=no -p 4242 ishan@34.75.6.116 <<EOL # Unquote so lines are expanded
 	cd ~/devchat
 	go build && echo Built
 	echo $SERVER_PASS | sudo -S pkill devchat && echo Killed
