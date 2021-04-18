769628eaf2abefc48c6412d728c7744b3e7ce7f8
Ishan Goel <igoel.mail@gmail.com>
2021-04-18 16:03:03 +0400

    AHHH found the bug

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo $PUBKEY > ~/.ssh/id_rsa.pub
 echo Wrote private key
 scp -P 4242 ./devchat.go go.sum go.mod ishan@34.75.6.116:~/devchat
 echo Copied files
-ssh -T -P 4242 -l ishan 34.75.6.116 <<'EOL'
+ssh -T -p 4242 ishan@34.75.6.116 <<'EOL'
 	cd ~/devchat
 	go build
 	echo Built
