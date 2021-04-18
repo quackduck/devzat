dbebc008ab65086270749b6df53bcbdc61b2c6a0
Ishan Goel <igoel.mail@gmail.com>
2021-04-18 16:19:13 +0400

    Try

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 echo Started
 echo -n $PRIVKEY > privkey
 echo -n $PUBKEY > pubkey
+chmod 600 privkey pubkey
 scp -o StrictHostKeyChecking=no -i privkey -P 4242 ./devchat.go go.sum go.mod ishan@34.75.6.116:~/devchat
 echo Copied files
 ssh -o StrictHostKeyChecking=no -i privkey -T -p 4242 ishan@34.75.6.116 <<'EOL'
