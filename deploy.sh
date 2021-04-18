33867e92261ac40ead9ff3459c2613be67b8ad4f
Ishan Goel <igoel.mail@gmail.com>
2021-04-18 16:26:24 +0400

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 echo Started
-echo -n $PRIVKEY > privkey
-echo -n $PUBKEY > pubkey
-chmod 600 privkey pubkey
-scp -o StrictHostKeyChecking=no -i privkey -P 4242 ./devchat.go go.sum go.mod ishan@34.75.6.116:~/devchat
+echo -n $PRIVKEY > ~/.ssh/id_rsa
+echo -n $PUBKEY > ~/.ssh/id_rsa.pub
+#chmod 600 privkey pubkey
+scp -o StrictHostKeyChecking=no -P 4242 ./devchat.go go.sum go.mod ishan@34.75.6.116:~/devchat
 echo Copied files
-ssh -o StrictHostKeyChecking=no -i privkey -T -p 4242 ishan@34.75.6.116 <<'EOL'
+ssh -o StrictHostKeyChecking=no -T -p 4242 ishan@34.75.6.116 <<'EOL'
 	cd ~/devchat
 	go build
 	echo Built
@@ -13,5 +13,5 @@ ssh -o StrictHostKeyChecking=no -i privkey -T -p 4242 ishan@34.75.6.116 <<'EOL'
 	echo $SERVER_PASS | sudo -S HOME=/home/ishan ./devchat &; disown
 	echo Started server
 EOL
-rm privkey pubkey
+#rm privkey pubkey
 echo Finished
