0b16f9f23a438914084c1130995ee5e705969d12
Ishan Goel <igoel.mail@gmail.com>
2021-04-18 16:35:10 +0400

    Please work

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 echo Started
 mkdir ~/.ssh
 echo $PRIVKEY > ~/.ssh/id_rsa
+sed -i -e "s#\\\\n#\n#g" ~/.ssh/id_rsa # https://titanwolf.org/Network/Articles/Article?AID=9397221a-10ca-49af-a8ca-5008c5192ae2#gsc.tab=0
 echo $PUBKEY > ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/*
 scp -o StrictHostKeyChecking=no -P 4242 ./devchat.go go.sum go.mod ishan@34.75.6.116:~/devchat
