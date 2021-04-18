d525baa91b19f83d2b8c4be762a8ff9ad5fd286a
Ishan Goel <igoel.mail@gmail.com>
2021-04-18 16:04:45 +0400

    Try

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo Started
 mkdir ~/.ssh
-echo $PRIVKEY > ~/.ssh/id_rsa
-echo $PUBKEY > ~/.ssh/id_rsa.pub
+echo -n $PRIVKEY > ~/.ssh/id_rsa
+echo -n $PUBKEY > ~/.ssh/id_rsa.pub
 echo Wrote private key
 scp -P 4242 ./devchat.go go.sum go.mod ishan@34.75.6.116:~/devchat
 echo Copied files
