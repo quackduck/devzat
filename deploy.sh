7a1ef4ea682335926b184c24b7a85809041d9796
Ishan Goel <igoel.mail@gmail.com>
2021-05-03 22:51:37 +0400

    Use new IP instead of domain

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo Started
-scp -o StrictHostKeyChecking=no -P 4242 ./devchat.go go.sum go.mod ishan@devzat.hackclub.com:~/devchat
+scp -o StrictHostKeyChecking=no -P 4242 ./devchat.go go.sum go.mod ubuntu@150.136.142.44:~/devchat
 echo Copied files
-ssh -o StrictHostKeyChecking=no -p 4242 ishan@devzat.hackclub.com <<EOL # Unquote so lines are expanded
+ssh -o StrictHostKeyChecking=no -p 4242 ubuntu@150.136.142.44 <<EOL # Unquote so lines are expanded
 	cd ~/devchat
 	go build && echo Built
 	echo $SERVER_PASS | sudo -S pkill devchat && echo Killed
