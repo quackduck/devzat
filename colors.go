f3557ca4534f347c82fd0e410fa7602a6d0fced5
Ishan Goel <igoel.mail@gmail.com>
2022-04-08 16:10:11 -0700

    Fix #111

--- colors.go
+++ colors.go
@@ -133,7 +133,6 @@ func (u *User) changeColor(colorName string) error {
 	u.Name, _ = applyColorToData(u.Name, u.Color, u.ColorBG) // error can be discarded as it has already been checked earlier
 
 	u.term.SetPrompt(u.Name + ": ")
-	saveBans()
 	return nil
 }
 
