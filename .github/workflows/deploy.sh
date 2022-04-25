01ac1a17d2da608c7c07d215a9597691cc590ea8
Ishan Goel <igoel.mail@gmail.com>
2022-04-24 21:30:28 -0700

    Fix deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,5 +1,5 @@
 echo Started
-scp -o StrictHostKeyChecking=no -P 4242 *.yml *.go go.sum go.mod ubuntu@150.136.142.44:~/devzat
+scp -o StrictHostKeyChecking=no -r -P 4242 *.yml *.go plugin go.sum go.mod ubuntu@150.136.142.44:~/devzat
 echo Copied files
 ssh -o StrictHostKeyChecking=no -p 4242 ubuntu@150.136.142.44 <<EOL # Unquote so lines are expanded
 	cd ~/devzat
