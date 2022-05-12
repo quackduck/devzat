0b48e9509461c6d04770d991e9db44a32d03c71b
PPTide <52575064+PPTide@users.noreply.github.com>
2022-05-12 17:36:07 +0200

    Revert "Add read-only Discord bridge"
    This reverts commit e15c44c4bc898ae2c554bf17fbf1b5b578b1e1a9.

--- devchat.go
+++ devchat.go
@@ -174,10 +174,8 @@ func (r *room) broadcast(senderName, msg string) {
 	}
 	if senderName != "" {
 		slackChan <- "[" + r.name + "] " + senderName + ": " + msg
-		discordChan <- "[" + r.name + "] " + senderName + ": " + msg
 	} else {
 		slackChan <- "[" + r.name + "] " + msg
-		discordChan <- "[" + r.name + "] " + msg
 	}
 	r.broadcastNoSlack(senderName, msg)
 }
