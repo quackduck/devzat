c0482a4a1480d985f971e8d5008933bc9412f560
Maxime Bouillot <maxime.bouillot@withings.com>
2022-05-13 21:25:34 +0200

    Removed go-away.
    It worked very poorly.

--- util.go
+++ util.go
@@ -4,10 +4,8 @@ import (
 	"bytes"
 	"crypto/sha256"
 	_ "embed"
-	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
-	"fmt"
 	"math"
 	"math/rand"
 	"os"
@@ -15,7 +13,6 @@ import (
 	"text/tabwriter"
 	"time"
 
-	goaway "github.com/TwiN/go-away"
 	"github.com/acarl005/stripansi"
 	markdown "github.com/quackduck/go-term-markdown"
 )
@@ -287,28 +284,9 @@ func shasum(s string) string {
 	return hex.EncodeToString(h[:])
 }
 
-var detector = goaway.NewProfanityDetector().WithSanitizeSpaces(false)
-
 func rmBadWords(text string) string {
-	if !Config.Censor {
-		return text
-	}
-	return detector.Censor(text)
-}
-
-func init() {
-	okayIshWords := []string{"ZnVjaw==", "Y3JhcA==", "c2hpdA==", "YXJzZQ==", "YXNz", "YnV0dA=="} // base 64 encoded okay-ish swears
-	for i := 0; i < len(goaway.DefaultProfanities); i++ {
-		for _, okayIshWord := range okayIshWords {
-			okayIshWordb, _ := base64.StdEncoding.DecodeString(okayIshWord)
-			if goaway.DefaultProfanities[i] == string(okayIshWordb) {
-				fmt.Println(string(okayIshWordb))
-				goaway.DefaultProfanities = append(goaway.DefaultProfanities[:i], goaway.DefaultProfanities[i+1:]...)
-				i-- // so we don't skip the next word
-				break
-			}
-		}
-	}
+    // TODO: fill this
+	return text
 }
 
 func holidaysCheck(u *User) {
