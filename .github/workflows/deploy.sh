3c6c82da951c7b286e00d87f958788de4abd459d
Ishan Goel <igoel.mail@gmail.com>
2022-05-31 00:53:44 +0000

    workflow changes

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,9 +1,9 @@
 echo Started
-scp -o StrictHostKeyChecking=no -r -P 4242 *.yml *.go plugin go.sum go.mod ubuntu@150.136.142.44:~/devzat
+scp -o StrictHostKeyChecking=no -r -P 4242 *.yml *.go rustrict_devzat plugin go.sum go.mod ubuntu@150.136.142.44:~/devzat
 echo Copied files
 ssh -o StrictHostKeyChecking=no -p 4242 ubuntu@150.136.142.44 <<EOL # Unquote so lines are expanded
 	cd ~/devzat
-	go build && echo Built
+	make && echo Built
 	echo $SERVER_PASS | sudo -S pkill devzat && echo Killed
 	sleep 2
 	echo $SERVER_PASS | sudo -S pkill -9 devzat && echo Killed with SIGKILL
