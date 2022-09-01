b69bdf59fa8bdfb19d2bb103ea7ff24e5bf122eb
Maxime Bouillot <maxime.bouillot@withings.com>
2022-09-01 13:22:17 +0200

    Added more test cases and fix dummy users.

--- devzat_test.go
+++ devzat_test.go
@@ -28,28 +28,41 @@ func makeDummyRoom() Room {
 	tim := &User{
 		Name: "tim",
 		term: dummyTerm,
+        ColorBG : "bg-off",
 	}
 	tim.changeColor("red")
 	ret.users = append(ret.users, tim)
 	tom := &User{
 		Name: "tom",
 		term: dummyTerm,
+        ColorBG : "bg-off",
 	}
 	tom.changeColor("blue")
 	ret.users = append(ret.users, tom)
 	timtom := &User{
 		Name: "timtom",
 		term: dummyTerm,
+        ColorBG : "bg-off",
 	}
 	timtom.changeColor("sky")
 	ret.users = append(ret.users, timtom)
 	timt := &User{
 		Name: "timt",
 		term: dummyTerm,
+        ColorBG : "bg-off",
 	}
 	timt.changeColor("coral")
 	ret.users = append(ret.users, timt)
 	return ret
+}
+
+func performTFM(t *testing.T, r Room, raw string, expected string) {
+    colored := r.findMention(raw)
+	t.Log(colored)
+	if colored != expected {
+		t.Log(expected)
+		t.Fail()
+	}
 }
 
 func TestFindMention(t *testing.T) {
@@ -57,11 +70,12 @@ func TestFindMention(t *testing.T) {
 	inputMsg := "@tim @tom @timtom @timt Hi!"
 	// Warning, the order the elements have been put in the dummy room affects the result of the test
 	expectedMsg := r.users[0].Name + " " + r.users[1].Name + " " + r.users[2].Name + " " + r.users[3].Name + " Hi!"
-	coloredMsg := r.findMention(inputMsg)
-	t.Log(coloredMsg)
-	if coloredMsg != expectedMsg {
-		t.Log(expectedMsg)
-		t.Fail()
-	}
-	//fmt.Println(r.findMention("@tim @tom @timt @timtom Hi!"))
+    performTFM(t, r, inputMsg, expectedMsg)
+    inputMsg = "@tim @fakemention"
+    expectedMsg = r.users[0].Name+" @fakemention"
+    performTFM(t, r, inputMsg, expectedMsg)
+    inputMsg = "  @tim  "
+    expectedMsg = "  "+r.users[0].Name+"  "
+    performTFM(t, r, inputMsg, expectedMsg)
 }
+
