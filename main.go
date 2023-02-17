ed79217334c4d82734b5a4176a2199a71e07a76d
Ishan Goel <igoel.mail@gmail.com>
2023-02-16 22:18:45 -0800

    remove deprecated call to rand.seed

--- main.go
+++ main.go
@@ -121,7 +121,6 @@ func main() {
 			Log.Println(err)
 		}
 	}()
-	rand.Seed(time.Now().Unix())
 	readBans()
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
