132a7582aeecdf734ecefa65b71384612e137d31
Maxime Bouillot <maxime.bouillot@withings.com>
2023-05-11 17:59:51 +0200

    Improve default prompt.

--- main.go
+++ main.go
@@ -421,7 +421,7 @@ func newUser(s ssh.Session) *User {
 		u.changeColor("bg-random") //nolint:errcheck // we know "bg-random" is a valid color
 	}
 
-	u.Prompt = u.Name + ":"
+	u.Prompt = "\\u:"
 	timeoutChan := make(chan bool)
 	timedOut := false
 	go func() { // timeout to minimize inactive connections
