de6ccdf56815d5445dec05928c917ad3350275c4
CaenJones <131218155+CaenJones@users.noreply.github.com>
2023-06-21 17:40:18 +0200

    Update censor.go to work with new censor script

--- censor.go
+++ censor.go
@@ -1,29 +1,60 @@
 package main
 
 import (
-	"encoding/base64"
-	goaway "github.com/TwiN/go-away"
+	"fmt"
+	"goaway"
 )
 
-var detector = goaway.NewProfanityDetector().WithSanitizeSpaces(false)
+func main() {
+	text := "This is a sample text with some words that may trigger the profanity filter."
 
-func rmBadWords(text string) string {
-	if !Config.Censor {
-		return text
-	}
-	return detector.Censor(text)
+	// Split the text into words
+	words := splitTextIntoWords(text)
+
+	// Filter out blocked words
+	filteredWords := goaway.FilterBlockedWords(words)
+
+	// Join the filtered words back into a string
+	filteredText := joinWordsIntoText(filteredWords)
+
+	fmt.Println(filteredText)
 }
 
-func init() {
-	okayIshWords := []string{"ZnVjaw==", "Y3JhcA==", "c2hpdA==", "YXJzZQ==", "YXNz", "YnV0dA==", "cGlzcw=="} // base 64 encoded okay-ish swears
-	for i := 0; i < len(goaway.DefaultProfanities); i++ {
-		for _, okayIshWord := range okayIshWords {
-			okayIshWordb, _ := base64.StdEncoding.DecodeString(okayIshWord)
-			if goaway.DefaultProfanities[i] == string(okayIshWordb) {
-				goaway.DefaultProfanities = append(goaway.DefaultProfanities[:i], goaway.DefaultProfanities[i+1:]...)
-				i-- // so we don't skip the next word
-				break
+// splitTextIntoWords splits the given text into a slice of words
+func splitTextIntoWords(text string) []string {
+	// Split the text by whitespace
+	words := make([]string, 0)
+	currentWord := ""
+	for _, char := range text {
+		if char == ' ' {
+			// Add the current word to the slice
+			if currentWord != "" {
+				words = append(words, currentWord)
+				currentWord = ""
 			}
+		} else {
+			// Append the character to the current word
+			currentWord += string(char)
 		}
 	}
+
+	// Add the last word to the slice (if any)
+	if currentWord != "" {
+		words = append(words, currentWord)
+	}
+
+	return words
+}
+
+// joinWordsIntoText joins the given slice of words into a string
+func joinWordsIntoText(words []string) string {
+	// Join the words with a space separator
+	text := ""
+	for i, word := range words {
+		if i > 0 {
+			text += " "
+		}
+		text += word
+	}
+	return text
 }
