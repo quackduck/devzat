4cc41905a0e3a8c93583abfb91b6f5983991329b
CaenJones <131218155+CaenJones@users.noreply.github.com>
2023-06-23 08:35:58 +0200

    Fix censor.go to work with new false positive config

---
+++ censor.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"goaway "github.com/CaenJones/goaway"
+)
+
+func main() {
+	text := "My stupid iphone broke on the last stupid day before retirement."
+
+	// Split the text into words
+	words := splitTextIntoWords(text)
+
+	// Filter out blocked words
+	filteredWords := goaway.FilterBlockedWords(words)
+
+	// Join the filtered words back into a string
+	filteredText := joinWordsIntoText(filteredWords)
+
+	fmt.Println(filteredText)
+}
+
+// splitTextIntoWords splits the given text into a slice of words
+func splitTextIntoWords(text string) []string {
+	// Split the text by whitespace
+	words := make([]string, 0)
+	currentWord := ""
+	for _, char := range text {
+		if char == ' ' {
+			// Add the current word to the slice
+			if currentWord != "" {
+				words = append(words, currentWord)
+				currentWord = ""
+			}
+		} else {
+			// Append the character to the current word
+			currentWord += string(char)
+		}
+	}
+
+	// Add the last word to the slice (if any)
+	if currentWord != "" {
+		words = append(words, currentWord)
+	}
+
+	return words
+}
+
+// joinWordsIntoText joins the given slice of words into a string
+func joinWordsIntoText(words []string) string {
+	// Join the words with a space separator
+	text := ""
+	for i, word := range words {
+		if i > 0 {
+			text += " "
+		}
+		text += word
+	}
+	return text
+}
