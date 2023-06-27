d9a8a2bf901668f467498b27194c7fcff7a0f422
Ishan Goel <igoel.mail@gmail.com>
2023-06-27 19:31:18 +0000

    simplify uptime

--- commands.go
+++ commands.go
@@ -807,20 +807,7 @@ func unameCMD(rest string, u *User) {
 	u.room.broadcast("", "Devzat "+unameCommit+": "+unameTime)
 }
 
-func floor(f float64) int {
-	return int(math.Floor(f))
-}
-
 func uptimeCMD(rest string, u *User) {
-	uptime := time.Since(StartupTime).Round(time.Second)
-	hours := floor(uptime.Hours())
-	minutes := floor(uptime.Minutes()) - (60 * hours)
-	seconds := floor(uptime.Seconds()) - (60 * (minutes + (60 * hours)))
-	daysStr := ""
-	if hours > 23 {
-		daysStr = fmt.Sprintf("%v days, ", hours/24)
-		hours = hours % 24
-	}
-	msg := fmt.Sprintf("up %s%02d:%02d:%02d", daysStr, hours, minutes, seconds)
-	u.room.broadcast("", msg)
+	uptime := time.Since(StartupTime)
+	u.room.broadcast("", fmt.Sprintf("up %v days, %02d:%02d:%02d", int(uptime.Hours() / 24), int(math.Mod(uptime.Hours(), 24)), int(math.Mod(uptime.Minutes(), 60)), int(math.Mod(uptime.Seconds(), 60))))
 }
