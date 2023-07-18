abb0b48160e7c0e5073a97367467312fd94ddb0d
Ishan Goel <igoel.mail@gmail.com>
2023-07-17 22:31:44 -0400

    Update twitter.go

--- twitter.go
+++ twitter.go
@@ -54,7 +54,6 @@ func sendCurrentUsersTwitterMessage() {
 		t, _, err := Client.Statuses.Update("People on Devzat rn: "+stripansi.Strip(fmt.Sprint(names))+"\nJoin em with \"ssh devzat.hackclub.com\"\nUptime: "+printPrettyDuration(time.Since(StartupTime)), nil)
 		if err != nil {
 			if !strings.Contains(err.Error(), "twitter: 187 Status is a duplicate.") {
-				// Log the error to the file using Log.Println
 				Log.Println("Twitter error:", err)
 			}
 			Log.Println("Got twitter err", err)
