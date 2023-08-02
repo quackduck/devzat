708b703bfd42ddfe3f13e5eb4656d8520ecb8acd
Ishan Goel <igoel.mail@gmail.com>
2023-08-02 16:27:58 -0400

    Add random delay, condense responses into fewer lines

--- commands.go
+++ commands.go
@@ -876,26 +876,13 @@ func neofetchCMD(_ string, u *User) {
 
 func EightBallCMD(_ string, u *User) {
 	responses := []string{
-		"It is certain, ",
-		"It is decidedly so, ",
-		"Without a doubt, ",
-		"Yes, definitely, ",
-		"You may rely on it, ",
-		"As I see it, yes, ",
-		"Most likely, ",
-		"Outlook good, ",
-		"Yes, ",
-		"Signs point to yes, ",
-		"Reply hazy, try again, ",
-		"Ask again later, ",
-		"Better not tell you now, ",
-		"Cannot predict now, ",
-		"Concentrate and ask again, ",
-		"Don't count on it, ",
-		"My reply is no, ",
-		"My sources say no, ",
-		"Outlook not so good, ",
+		"It is certain, ", "It is decidedly so, ", "Without a doubt, ", "Yes, definitely, ",
+		"You may rely on it, ", "As I see it, yes, ", "Most likely, ", "Outlook good, ",
+		"Yes, ", "Signs point to yes, ", "Reply hazy, try again, ", "Ask again later, ",
+		"Better not tell you now, ", "Cannot predict now, ", "Concentrate and ask again, ",
+		"Don't count on it, ", "My reply is no, ", "My sources say no, ", "Outlook not so good, ",
 		"Very doubtful, ",
 	}
+	time.Sleep(time.Second*rand.Intn(10))
 	u.room.broadcast("8ball", responses[rand.Intn(len(responses))] + u.Name)
 }
