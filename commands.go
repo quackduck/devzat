42d758d45953f103ac77aadaac254fce0718dfd0
Caen Jones <131218155+CaenJones@users.noreply.github.com>
2023-08-19 12:13:03 -0400

    Update commands.go

--- commands.go
+++ commands.go
@@ -492,7 +492,7 @@ func promptCMD(line string, u *User) {
     u.formatPrompt()
 
     if line == "" {
-        User.broadcast(Devbot, "Your prompt is now empty. Did you mean to get more info about your prompt? Run man prompt for more info")
+        MainRoom.broadcast(Devbot, "Your prompt is now empty. Did you mean to get more info about your prompt? Run man prompt for more info")
     }
 }
 
