5b7c7528d6443be5175c69fc8a17f5b5d4bdd549
PPTide <52575064+PPTide@users.noreply.github.com>
2025-01-13 09:36:58 +0100

    Update dependencies
    This is needed for #248

--- devzatapi/example/main.go
+++ devzatapi/example/main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	s, err := api.NewSession("devzat.hackclub.com:5556", os.Getenv("DEVZAT_TOKEN"))
+	//s, err := api.NewSession("devzat.hackclub.com:5556", os.Getenv("DEVZAT_TOKEN"))
+	s, err := api.NewSession("localhost:5556", "dvz@R1HkT12cOvzc4nbPNUnE1xEdkHIhtnKGQkotDPzCOAc=")
 	if err != nil {
 		panic(err)
 	}
