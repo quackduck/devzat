c0b934ff10a2ae88198cb271da73c44a59dc3124
Maxime Bouillot <maxbouillot@gmail.com>
2025-10-15 20:57:09 +0200

    Add copy of log lines to retrieve latter.

--- logs.go
+++ logs.go
@@ -1,7 +1,34 @@
 package main
 
+import (
+	"sync"
+)
+
+const adminLogSize = 4
+
+// This struct contains copied log lines meant to be read by admins.
+// They are stored as a circular buffer for ease of write.
+type adminLog struct {
+	logLines         [adminLogSize]string
+	writeIndex       int
+	totalWrittenLines int
+	lock             sync.Mutex
+}
+
+var globalAdminLog = adminLog{totalWrittenLines: 0, writeIndex: 0}
+
+func (al *adminLog) addLine(p []byte) {
+	al.lock.Lock()
+	defer al.lock.Unlock()
+
+	al.logLines[al.writeIndex%adminLogSize] = string(p)
+	al.writeIndex++
+	al.totalWrittenLines++
+}
+
 type AdminLogWriter struct{}
 
-func (alw AdminLogWriter) Write(p []byte) (int, error) {
+func (_ AdminLogWriter) Write(p []byte) (int, error) {
+	globalAdminLog.addLine(p)
 	return len(p), nil
 }
