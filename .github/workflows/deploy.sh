fbebb8456fac68fce6643081fb8b4c8774ec0aa0
Maxime Bouillot <maxbouillot@gmail.com>
2026-06-25 22:24:33 +0200

    Use systemctl in deploy script.
    This matches the new configuration on the server.
    It's more reliable that fire-and-forget disowned
    jobs.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -4,10 +4,12 @@ echo Copied files
 ssh -o StrictHostKeyChecking=no -p 4242 ubuntu@150.136.142.44 <<EOL # Unquote so lines are expanded
 	cd ~/devzat
 	go build -ldflags "-X 'main.unameCommit=$(git rev-parse HEAD)' -X 'main.unameTime=$(date)'" && echo Built
+	echo $SERVER_PASS | sudo systemctl stop devzat
+	sleep 2
 	echo $SERVER_PASS | sudo -S pkill devzat && echo Killed
 	sleep 2
 	echo $SERVER_PASS | sudo -S pkill -9 devzat && echo Killed with SIGKILL
-	echo $SERVER_PASS | nohup sudo -S GOMAXPROCS=2 DEVZAT_CONFIG=mainserver.yml ./devzat > /dev/null 2>stderr </dev/null &
+	echo $SERVER_PASS | sudo systemctl start devzat
 	echo Started server
 	disown
 	exit
